f14e4d3c636efd228acc79a9563665c3df9385b4
Stupid-Benz <66051526+Stupid-Benz@users.noreply.github.com>
2020-05-30 17:06:31 +0800

    Add files via upload

---
+++ Stupid Benz Bot 1.2/Stupid Benz Bot/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Stupid_Benz_Bot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        public DiscordSocketClient Client;
+        private CommandService Commands;
+        private IServiceProvider Services;
+
+        public async Task RunBotAsync()
+        {
+            Client = new DiscordSocketClient();
+            Commands = new CommandService();
+
+            Services = new ServiceCollection()
+                .AddSingleton(Client)
+                .AddSingleton(Commands)
+                .BuildServiceProvider();
+
+            string token = "NjkxMTExMjc5MzgwNjYwMjU0.XtDeAw.HXLFvqWgNUHECOj3k4mE0e6bCVc";
+
+            Client.Log += Client_Log;
+
+            await RegisterCommandAsync();
+
+            await Client.LoginAsync(TokenType.Bot, token);
+
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            Client.MessageReceived += HandleCommandAsync;
+
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), Services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(Client, message);
+            if (message.Author.IsBot)
+            {
+                return;
+            }
+
+            int argPos = 0;
+            if (message.HasStringPrefix("=", ref argPos))
+            {
+                var result = await Commands.ExecuteAsync(context, argPos, Services);
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
