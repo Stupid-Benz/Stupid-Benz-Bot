b81f0df9841038ee1832b379a8fb86fe6831ae86
Stupid-Benz <66051526+Stupid-Benz@users.noreply.github.com>
2020-06-21 10:46:29 +0800

    1.4.0

---
+++ Stupid Benz Bot 1.4.0/Stupid Benz Bot Chat Bot/Modules/Japanese Reply.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using Discord.Commands;
+using Discord;
+using Discord.WebSocket;
+using Discord.Rest;
+using System.Threading.Channels;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+
+namespace Stupid_Benz_Bot_Chat_Bot.Modules
+{
+    public class Japanese_Reply : ModuleBase<SocketCommandContext>
+    {
+        [Command("Konnichiwa")]
+        [Alias("こんにちは", "konnichiwa")]
+        public async Task Konnichiwa()
+        {
+            await ReplyAsync("こんにちは");
+        }
+
+        [Command("Hajimemashite")]
+        [Alias("はじめまして", "hajimemashite")]
+        public async Task Hajimemashite()
+        {
+            await ReplyAsync("はじめまして，私は御坂美琴です，どうぞよろしくお願いします"
+                + Context.Message.Author.Mention);
+        }
+
+        [Command("Ohayogozayimasu")]
+        [Alias("おはようごぢいます", "おはよう", "ohayogozayimasu", "Ohaayo", "ohayo")]
+        public async Task Ohayogozayimasu()
+        {
+            await ReplyAsync("おはよう" + Context.Message.Author.Mention);
+        }
+    }
+}
---
+++ Stupid Benz Bot 1.4.0/Stupid Benz Bot Chat Bot/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Stupid_Benz_Bot_Chat_Bot.Modules
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+        private IServiceProvider Services;
+
+        public async Task RunBotAsync()
+        {
+            Client = new DiscordSocketClient();
+            Commands = new CommandService();
+
+            Services = new ServiceCollection()
+                .AddSingleton(Client)
+                .AddSingleton(Commands)
+                .BuildServiceProvider();
+
+            string token = "NjkxMTExMjc5MzgwNjYwMjU0.Xu4S1A.KeQw02GA9FB37nlcjxZXlB_chD0";
+
+            Client.Log += Client_Log;
+
+            await RegisterCommandAsync();
+
+            await Client.LoginAsync(TokenType.Bot, token);
+
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+        private Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            Client.MessageReceived += HandleCommandAsync;
+
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), Services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(Client, message);
+
+            ulong id = context.Channel.Id;
+            ulong chatid = 721580073555329049;
+            if (id == chatid)
+            {
+                if (message.Author.IsBot)
+                {
+                    return;
+                }
+
+                int argPos = 0;
+
+                if (message.HasStringPrefix("", ref argPos))
+                {
+                    var result = await Commands.ExecuteAsync(context, argPos, Services);
+                    if (result.ErrorReason == "Unknown command." & message.HasStringPrefix("=", ref argPos) == false)
+                    {
+                        await context.Channel.SendMessageAsync("Sorry, I don't what you mean");
+                        Console.WriteLine(result.ErrorReason);
+                    }
+                    else if (message.HasStringPrefix("=", ref argPos) == true)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        await context.Channel.SendMessageAsync(result.ErrorReason);
+                        Console.WriteLine(result.ErrorReason);
+                    }
+                }
+                
+                
+            }
+        }
+    }
+}
