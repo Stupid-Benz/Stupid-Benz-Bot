2fc94c74c48b2e50c0e5cf2873f4f5131802d716
Stupid-Benz <66051526+Stupid-Benz@users.noreply.github.com>
2020-07-03 18:20:39 +0800

    Update

---
+++ Python/KCCS-Official-Rank 1.2/website.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify
+import multiprocessing as mulP
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    result = ''
+    try:
+        f = open('./html/index.html', 'r')
+        result = f.read()
+        f.close()
+    except:
+        result = 'error fetching data.'
+    return result
+
+
+@app.route('/style.css')
+def css():
+    result = ''
+    try:
+        f = open('./html/style.css', 'r')
+        result = f.read()
+        f.close()
+    except:
+        return 'error fetching data.', 400, {
+            'Content-Type': 'text/plain; charset=utf-8'
+        }
+    return result, 200, {'Content-Type': 'text/css; charset=utf-8'}
+
+
+@app.route('/script.js')
+def js():
+    result = ''
+    try:
+        f = open('./html/script.js', 'r')
+        result = f.read()
+        f.close()
+    except:
+        result = 'error fetching data.', 400, {
+            'Content-Type': 'text/plain; charset=utf-8'
+        }
+    return result, 200, {'Content-Type': 'text/javascript; charset=utf-8'}
+
+
+@app.route('/user.json')
+def json():
+    result = ''
+    try:
+        f = open('./user.json', 'r')
+        result = f.read()
+        f.close()
+    except:
+        result = 'error fetching data.', 400, {
+            'Content-Type': 'text/plain; charset=utf-8'
+        }
+    return result, 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+
+def init():
+    return mulP.Process(target=app.run, args=('0.0.0.0', ))
