6ba1ac6cc70367177e894d542f25ba118dfdd2a2
Stupid-Benz <66051526+Stupid-Benz@users.noreply.github.com>
2020-07-03 18:30:16 +0800

    Update

--- Python/KCCS-Official-Rank 1.1/main.py
+++ Python/KCCS-Official-Rank 1.1/main.py
@@ -3,16 +3,16 @@ from time import sleep
 
 
 def a():
-	while True:
-		print(end="")
-		sleep(300)
+    while True:
+        print(end="")
+        sleep(300)
 
 
 t = threading.Thread(target=a)
 t.start()
 try:
-	page = website.init()
-	page.start()
+    page = website.init()
+    page.start()
 except Exception as e:
-	print(e)
+    print(e)
 import bot
