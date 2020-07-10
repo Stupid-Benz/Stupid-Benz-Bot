1c93a371f1469c3d62df2370fa79113a2da43dae
Stupid-Benz <66051526+Stupid-Benz@users.noreply.github.com>
2020-07-10 18:12:27 +0800

    Update 1.4.2 and 1.5.0

---
+++ Stupid Benz Bot 1.4.2/Modules/Help Commands.cs
@@ -0,0 +1,261 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Stupid_Benz_Bot.Modules
+{
+    [Group("help")]
+    public class Help_Commands : ModuleBase<SocketCommandContext>
+    {
+
+        [Command]
+        public async Task Help()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Command List"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("Ping", "Give Ping Time", true)
+                .AddField("Avatar", "Give the Avatar", true)
+                .AddField("UserInfo", "Give the user information", true)
+                .AddField("Spam", "Spamming", true)
+                .AddField("Sin", "Sine Function", true)
+                .AddField("Cos", "Cosine Function", true)
+                .AddField("Tan", "Tangent Function", true)
+                .AddField("Surd", "Surd Function", true)
+                .AddField("SSurd", "Simpify Surd Function", true)
+                .AddField("Sq", "Square Function", true)
+                .AddField("Cu", "Cubic Function", true)
+                .AddField("Quad", "Quadratic Function", true)
+                .AddField("Fac", "Find all Factor Function", true)
+                .AddField("PF", "Prime Factorization Funtion", true);
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("ping")]
+        public async Task HelpPing()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Ping"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=ping")
+                .AddField("Usage", "Give Ping Time");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("spam")]
+        public async Task HelpSpam()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Spam"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=spam [Text] [Times]")
+                .AddField("Usage", "Spamming")
+                .AddField("Caution!", "Don\'t over 30");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("avatar")]
+        public async Task HelpAvatar()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Avatar"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=avatar [Member Mention] [Size of the avatar in pixel]")
+                .AddField("Usage", "Give the Avatar")
+                .AddField("Caution!", "The size of the avatar need to be 128, 256, 512, 1024 (normal), 2048 (2k), 4096 (4k)")
+                .AddField("Alias", "Ava");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("userinfo")]
+        public async Task HelpSay()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "User Info"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=userinfo [Member Mention]")
+                .AddField("Usage", "Give the user information")
+                .AddField("Alias", "User");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("sin")]
+        public async Task HelpSin()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: Sin"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=sin [Degree]")
+                .AddField("Usage", "Sine Function")
+                .AddField("Alias", "Sine");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("cos")]
+        public async Task HelpCos()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: Cos"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=cos [Degree]")
+                .AddField("Usage", "Cosine Function")
+                .AddField("Alias", "Cosine");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("tan")]
+        public async Task HelpTan()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: Tan"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=tan [Degree]")
+                .AddField("Usage", "Tangent Function")
+                .AddField("Alias", "Tangent");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("surd")]
+        public async Task HelpSurd()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: Surd"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=surd [Degree]")
+                .AddField("Usage", "Surd Function");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("ssurd")]
+        public async Task HelpSSurd()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: SSurd"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=sin [Degree]")
+                .AddField("Usage", "Simplify Surd Function")
+                .AddField("Alias", "SimplifySurd");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("sq")]
+        public async Task HelpSq()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: Sq"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=sq [Degree]")
+                .AddField("Usage", "Square Function")
+                .AddField("Alias", "Square");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("cu")]
+        public async Task HelpCu()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: Cu"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=cu [Degree]")
+                .AddField("Usage", "Cubic Function")
+                .AddField("Alias", "Cubic");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("quad")]
+        public async Task HelpQuad()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: Quad"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=quad [Degree]")
+                .AddField("Usage", "Quadratic Function")
+                .AddField("Alias", "Quadratic");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("fac")]
+        public async Task HelpFac()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: Fac"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=fac [Degree]")
+                .AddField("Usage", "Find all Factor Function")
+                .AddField("Alias", "Factor");
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("pf")]
+        public async Task HelpPf()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Mathematics: PF"
+            };
+            embed.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .AddField("How to use", "=sin [Degree]")
+                .AddField("Usage", "Prime Factorization Function")
+                .AddField("Alias", "PrimeFactorize, PrimeFactorization");
+            await ReplyAsync("", false, embed.Build());
+        }
+    }
+}
---
+++ Stupid Benz Bot 1.5.0/Modules/Commands.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+using Discord.WebSocket;
+
+namespace Stupid_Benz_Bot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Pong!"
+            };
+            embed.AddField("Latency", $"The latency is **{PingTime()}** ms.")
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .WithAuthor(Context.Client.CurrentUser);
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        public string PingTime()
+        {
+            string cSec = Context.Message.Timestamp.Second.ToString();
+            string cMs = Context.Message.Timestamp.Millisecond.ToString();
+            string nSec = DateTime.Now.Second.ToString();
+            string nMs = DateTime.Now.Millisecond.ToString();
+            float cTime = float.Parse(cSec) * 1000
+                + float.Parse(cMs);
+            float nTime = float.Parse(nSec) * 1000
+                + float.Parse(nMs);
+            string pingTime = (nTime - cTime).ToString();
+            return pingTime;
+        }
+
+        //Enter "=test"
+        [Command("test")]
+        public async Task Test()
+        {
+            Random rnd = new Random();
+            string[] Ans =
+            {
+                "No Test! You Stupid ", "Once upon a time, there have **NO TEST** "
+            };
+            string reply = Ans[rnd.Next(Ans.Length)];
+            await ReplyAsync(reply + Context.Message.Author.Mention);
+        }
+
+        [Command("userinfo")]
+        [Alias("user")]
+        public async Task UserInfoAsync(SocketUser user = null)
+        {
+            if (user == null)
+            {
+                user = Context.User;
+            }
+            var embed = new EmbedBuilder
+            {
+                Title = $"{user.Username}'s Info"
+            };
+            embed.AddField("Name", user.Username)
+                .AddField("ID", user.Id)
+                .WithImageUrl(user.GetAvatarUrl(ImageFormat.Png, size: 1024))
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .WithAuthor(Context.Client.CurrentUser);
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+        [Command("avatar")]
+        [Alias("ava")]
+        public async Task Avatar(SocketUser user = null, ushort size = 1024)
+        {
+            if (user == null)
+            {
+                user = Context.User;
+            }
+            var userAvatar = user.GetAvatarUrl(ImageFormat.Png, size);
+            var embed = new EmbedBuilder();
+            embed.WithImageUrl(userAvatar)
+                .WithCurrentTimestamp()
+                .WithColor(Color.Blue)
+                .WithAuthor(Context.Client.CurrentUser)
+                .WithTitle(user.Username + "\'s Avatar");
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+
+
+        [Command("spam")]
+        public async Task Spamming(string sentence, int time)
+        {
+            if (time <= 30)
+            {
+                int i = 0;
+                while (i < time)
+                {
+                    await ReplyAsync(sentence);
+                    i++;
+                }
+                await ReplyAsync("Finished!");
+            }
+            else
+            {
+                throw new TimeoutException("System.TimeoutException: Too long");
+            }
+        }
+
+        [Command("bot-info")]
+        [Alias("info")]
+        public async Task BotInfo()
+        {
+            var bot = Context.Client.CurrentUser;
+            var embed = new EmbedBuilder
+            {
+                Title = $"{bot.Username}'s Info"
+            };
+            embed.AddField("Name", bot.Username)
+                .AddField("ID", bot.Id)
+                .WithImageUrl(bot.GetAvatarUrl(ImageFormat.Png, size: 1024))
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .WithAuthor(Context.Client.CurrentUser);
+            await Context.Channel.SendMessageAsync("```md\nMisaka Mikoto is going to have new version\n## [1.5.0]\n### Update\n- New Mode For `Sin` `Cos` `Tan` Function\n- `Base` Command\n- `!(Factorial)` `nCr` `nPr` Command\n\n### Repair\n- Show errors```", false, embed.Build());
+        }
+    }
+}
