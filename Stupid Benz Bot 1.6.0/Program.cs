038444596c6feda0c0dca400da3d2d031db7ee27
Stupid-Benz <66051526+Stupid-Benz@users.noreply.github.com>
2020-07-16 16:08:53 +0800

    Update 1.6.0

---
+++ Stupid Benz Bot 1.6.0/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Stupid_Benz_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+        private IServiceProvider Services;
+        private IConfiguration Config;
+
+        public async Task RunBotAsync()
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+            Commands = new CommandService();
+            Config = BuildConfig();
+            Services = new ServiceCollection()
+                .AddSingleton(Client)
+                .AddSingleton(Commands)
+                .AddSingleton(Config)
+                .BuildServiceProvider();
+
+            Client.Log += Client_Log;
+
+            Client.Ready += Client_Ready;
+
+            await RegisterCommandAsync();
+
+            await Client.LoginAsync(TokenType.Bot, Config["token"]);
+
+            await Client.StartAsync();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private IConfiguration BuildConfig()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json")
+                .Build();
+        }
+
+        private Task Client_Log(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        private Task Client_Ready()
+        {
+            Console.WriteLine($"{DateTime.Now.ToString().Remove(0, 11)} Gateway     Login as {Client.CurrentUser.Username} - {Client.CurrentUser.Id}");
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            Client.MessageReceived += HandleCommandAsync;
+
+            Client.MessageUpdated += HandleUpdateAsync;
+
+            Client.UserJoined += UserJoin;
+
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), Services);
+        }
+
+        private async Task UserJoin(SocketGuildUser user)
+        {
+            var channel = Client.GetChannel(Convert.ToUInt64(Config["announcements-id"])) as IMessageChannel;
+            await channel.SendMessageAsync($"Hey {user.Mention}, Welcome to {user.Guild.Name}\n\nYou will be muted for 3 minutes so that you may have time to read the **rules** and explore the server.\n\nHave Fun!");
+        }
+
+        private async Task HandleUpdateAsync(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel messageChannel)
+        {
+            var msg = await before.GetOrDownloadAsync();
+            var message = after as SocketUserMessage;
+            var context = new SocketCommandContext(Client, message);
+            if (message.Author.IsBot)
+            {
+                return;
+            }
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("=", ref argPos))
+            {
+                var result = await Commands.ExecuteAsync(context, argPos, Services);
+                if (!result.IsSuccess)
+                {
+                    await context.Channel.SendMessageAsync("```" + result.ErrorReason + "```");
+                    Console.WriteLine(result.ErrorReason);
+                }
+                var channel = Client.GetChannel(Convert.ToUInt64(Config["console-id"])) as IMessageChannel;
+                await channel.SendMessageAsync($"[{message.CreatedAt.DateTime.ToLocalTime()}] {context.Channel} {message.Author.Username} has edited command: `{msg}` ==> `{message.Content}`");
+
+            }
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(Client, message);
+            if (message.Author.IsBot)
+            {
+                return;
+            }
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("=", ref argPos))
+            {
+                var result = await Commands.ExecuteAsync(context, argPos, Services);
+                if (!result.IsSuccess)
+                {
+                    await context.Channel.SendMessageAsync("```" + result.ErrorReason + "```");
+                    Console.WriteLine(result.ErrorReason);
+                }
+                var channel = Client.GetChannel(Convert.ToUInt64(Config["console-id"])) as IMessageChannel;
+                await channel.SendMessageAsync($"[{message.CreatedAt.DateTime.ToLocalTime()}] {context.Channel} {message.Author.Username} has issued command: `{message.Content}`");
+
+            }
+        }
+    }
+}
